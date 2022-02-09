074914950224d46adf7154177292aa85111fad93
Didkarbit <powerfixprofi@gmail.com>
2022-02-09 11:55:30 +0200

    Added test file

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+function Test() {
+
+    return (
+    <div>
+        Test
+    </div>
+    )
+}
+
+export default Test
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData =  [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum omnis voluptas neque praesentium amet ad mollitia alias doloremque modi similique',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum omnis voluptas neque praesentium amet ad mollitia alias doloremque modi similique',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum omnis voluptas neque praesentium amet ad mollitia alias doloremque modi similique',
+    },
+]
+
+export default FeedbackData
